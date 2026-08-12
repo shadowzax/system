88b8624544eb5f035af0c13f0b17af55545bdedd
shadowzax <xshadowza@gmail.com>
2026-08-13 02:33:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -111,6 +111,20 @@ app.get("/", (req, res) => {
     });
 });
 */
+app.get("/1", (req, res) => {
+    res.render("../uploads/outgoing", {
+        title: "",
+        description: ""
+    });
+});
+
+app.get("/2", (req, res) => {
+    res.render("../uploads/test", {
+        title: "",
+        description: ""
+    });
+});
+
 app.get("/login", (req, res) => {
     if (req.isAuthenticated) {
         return res.redirect("/");
